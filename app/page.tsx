9874ed94f4186cd00d6bc5fca9ef07cfffddd3e9
Aaban Saad <aabansaad@gmail.com>
2025-05-24 21:08:56 +0600

    tagline

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="font-bold text-6xl">Chatpoka</h1>
+        <h2>Intelligent Chatbots for Automating Business Processes</h2>
         <p className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           We are launching soon...
         </p>
